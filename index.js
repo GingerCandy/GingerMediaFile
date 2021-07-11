507a3a1175d08b4725156881bb6ff825a5a39dd9
BimoSora <bimosora3@gmail.com>
2021-07-11 22:58:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ bot.action('POP',(ctx)=>{
 //help
 
 bot.command('/help',(ctx)=>{
-    ctx.reply(`Halo <b>${ctx.from.first_name}</b> \n\nAnda dapat mengirimi saya file dan saya akan menyimpan dan membagikan tautan untuk file itu untuk digunakan di dalam telegram\nAnda juga dapat menggunakan saya untuk mencari file yang disumbangkan oleh berbagai pengguna\n\n(<code>Anggap ini sebagai versi awal setelah memperbaiki tertentu bug kami akan membuat bot ini opensource</code>)`,{
+    ctx.reply(`Halo <b>${ctx.from.first_name}</b> \n\nAnda dapat mengirimi saya file dan saya akan menyimpan dan membagikan tautan untuk file itu untuk digunakan di dalam telegram\nAnda juga dapat menggunakan saya untuk mencari file yang disumbangkan oleh berbagai pengguna.`,{
         parse_mode:'HTML',\n
         reply_markup:{
             inline_keyboard:[

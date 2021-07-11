86a20aa52de585be31fa4669536f2c5b031bbd5d
BimoSora <bimosora3@gmail.com>
2021-07-12 05:48:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -50,10 +50,10 @@ bot.start(async(ctx)=>{
         file = await saver.getFile(query).then((res)=>{
             console.log(res);
             if(res.type=='video'){
-                ctx.replyWithVideo(res.file_id,{caption:`<b>${res.caption}</b>`,
+                ctx.replyWithVideo(res.file_id,{caption: `<b>${res.caption}</b>`,
             parse_mode:'HTML'})
             }else{
-                ctx.replyWithDocument(res.file_id,{caption:`<b>${res.caption}</b>`,
+                ctx.replyWithDocument(res.file_id,{caption: `<b>${res.caption}</b>`,
             parse_mode:'HTML'})
             }
             
@@ -157,7 +157,7 @@ bot.command('send',async(ctx)=>{
 
                 }
             }
-            ctx.reply(`<b>✅ Jumlah pengguna aktif:</b>${userId.length - totalFail.length}\n❌ <b>Total siaran yang gagal:</b>${totalFail.length}`,{
+            ctx.reply(`✅ <b>Jumlah pengguna aktif:</b> ${userId.length - totalFail.length}\n❌ <b>Total siaran yang gagal:</b> ${totalFail.length}`,{
                 parse_mode:'HTML'
             })
 
@@ -234,7 +234,7 @@ bot.on('document', async (ctx) => {
             ctx.reply(`https://t.me/${process.env.BOTUSERNAME}?start=${document.file_unique_id}`)
             ctx.replyWithDocument(document.file_id, {
                 chat_id: process.env.LOG_CHANNEL,
-                caption: `${ctx.message.caption}\n\n\nDari:${ctx.from.id}\nNama depan:${ctx.from.first_name}\nID file:${document.file_id}`
+                caption: `${ctx.message.caption}\n\n\nDari: ${ctx.from.id}\nNama depan: ${ctx.from.first_name}\nID file: ${document.file_id}`
 
             })
         }
@@ -267,7 +267,7 @@ bot.on('video', async(ctx) => {
             ctx.reply(`https://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`)
             ctx.replyWithVideo(video.file_id, {
                 chat_id: process.env.LOG_CHANNEL,
-                caption: `${ctx.message.caption}\n\n\nDari:${ctx.from.id}\nNama depan:${ctx.from.first_name}\nID file:${document.file_id}`
+                caption: `${ctx.message.caption}\n\n\nDari: ${ctx.from.id}\nNama depan: ${ctx.from.first_name}\nID file: ${document.file_id}`
             })
         }
     })
@@ -298,7 +298,7 @@ bot.on('audio', async(ctx) => {
             ctx.reply(`https://t.me/${process.env.BOTUSERNAME}?start=${audio.file_unique_id}`)
             ctx.replyWithDocument(audio.file_id, {
                 chat_id: process.env.LOG_CHANNEL,
-                caption: `${ctx.message.caption}\n\n\nDari:${ctx.from.id}\nNama depan:${ctx.from.first_name}\nID file:${document.file_id}`
+                caption: `${ctx.message.caption}\n\n\nDari: ${ctx.from.id}\nNama depan: ${ctx.from.first_name}\nID file: ${document.file_id}`
             })
         }
     })

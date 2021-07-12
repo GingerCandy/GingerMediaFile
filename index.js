4043230316dd512518821ee1afef79ad8a421c31
BimoSora <bimosora3@gmail.com>
2021-07-12 18:30:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ bot.start(async(ctx)=>{
                 ctx.replyWithVideo(res.file_id,{caption: `${res.caption} \n\n<b>Selamat menikmati.</b>`,
             parse_mode:'HTML'})
             }else if(res.type=='photo'){
-                if(photo[1].file_id == NULL){
+                if(ctx.reply(photo[1].file_id) == NULL){
                   ctx.replyWithPhoto(res.file_id,{caption: `\n\n<b>Selamat menikmati.</b>`,
                   parse_mode:'HTML'})
                 }else{

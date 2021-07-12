333ef9195509fb2513ae6d4b6b0eac3cc2d514d9
BimoSora <bimosora3@gmail.com>
2021-07-13 06:16:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.start(async(ctx)=>{
     if(length == 1){
         //ctx.replyWithPhoto('https://images4.imagebam.com/3e/67/3f/ME1WOMT_o.jpg',{caption: `<b>Selamat menikmati.</b>`,
             parse_mode:'HTML'})
-        ctx.reply('https://images4.imagebam.com/3e/67/3f/ME1WOMT_o.jpg',{caption: `${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,{
+        ctx.replyWithPhoto('https://images4.imagebam.com/3e/67/3f/ME1WOMT_o.jpg',{caption: `${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,{
             parse_mode:'HTML',
             reply_markup:{
                 inline_keyboard:[

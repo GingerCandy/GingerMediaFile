9ddbb8a688bfa026d2584c83e020effecff43b04
BimoSora <bimosora3@gmail.com>
2021-07-13 06:39:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ bot.start(async(ctx)=>{
     //welcoming message on /start and if there is a query available we can send files
 
     if(length == 1){
-        ctx.replyWithPhoto('https://images4.imagebam.com/3e/67/3f/ME1WOMT_o.jpg',{caption: `<b>Selamat menikmati apa yang kami berikan.</b>`})
+        ctx.replyWithPhoto('https://images4.imagebam.com/3e/67/3f/ME1WOMT_o.jpg',{caption: `<b>Selamat menikmati apa yang kami berikan.</b>`,
+          parse_mode:'HTML'
+        })
         ctx.reply(`${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,{
             parse_mode:'HTML',
             reply_markup:{

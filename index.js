7b33e78aa6c0e2a98cfe1f6e4a5604386453320e
BimoSora <bimosora3@gmail.com>
2021-07-13 13:27:54 +0700

    Upload file

--- index.js
+++ index.js
@@ -35,7 +35,14 @@ bot.start(async(ctx)=>{
     
     var member = await bot.telegram.getChatMember(-1001250398726, ctx.from.id)
     if (!member || member.status == 'left'){
-        ctx.reply(`${ctx.from.first_name} \n\n Anda belum masuk join, silakan join dulu!`)
+        ctx.reply(`${ctx.from.first_name} \n\n Anda belum masuk join, silakan join dulu!`,
+        parse_mode:'HTML',
+        reply_markup:{
+            inline_keyboard:[
+                [{text:'Gabung Channel', url: 'https://t.me/gingercandyfiles'}]
+            ]
+        }
+        )
     }else{
 
     //welcoming message on /start and if there is a query available we can send files

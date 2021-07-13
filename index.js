eb8cdf47ece79e9ee8542900822f5d64103f541d
BimoSora <bimosora3@gmail.com>
2021-07-13 07:49:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ bot.start(async(ctx)=>{
     //welcoming message on /start and if there is a query available we can send files
 
     if(length == 1){
-        userpic = await saver.getUserProfilePhotos(userId, 0, 1).then((res)=>{
+        userpic = await saver.getUserProfilePhotos().then((res)=>{
         ctx.replyWithPhoto(chat.id,res.photos[0][0].file_id,{caption: `${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,
             parse_mode:'HTML',
             reply_markup:{

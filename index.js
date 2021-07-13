84c8d3a9b7ccda5214265670a7bcdf564de4c969
BimoSora <bimosora3@gmail.com>
2021-07-13 13:17:47 +0700

    Upload file

--- index.js
+++ index.js
@@ -35,7 +35,13 @@ bot.start(async(ctx)=>{
     
     var member = await bot.telegram.getChatMember(-1001250398726, ctx.from.id)
     if (!member || member.status == 'left'){
-        ctx.reply(`belum join`)
+        ctx.reply(`${ctx.from.first_name} \n\nSilakan join dulu!.`,{
+            parse_mode:'HTML',
+            reply_markup:{
+                inline_keyboard:[
+                    [{text:'Join dulu yuk!', url: 'https://t.me/joinchat/sJHfeRe7SQU3YjNh'}]
+                ]
+            }
     }else{
 
     //welcoming message on /start and if there is a query available we can send files

df38d7a6c75c60fc4ed1c268720d69925db8a3e6
BimoSora <bimosora3@gmail.com>
2021-07-13 07:42:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ bot.start(async(ctx)=>{
     //welcoming message on /start and if there is a query available we can send files
 
     if(length == 1){
-        ctx.telegram.getUserProfilePhotos(userId, 0, 1).then(function(data){
-        ctx.replyWithPhoto(chat.id,data.photos[0][0].file_id,{caption: `${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,
+        userpic = await saver.getUserProfilePhotos(userId, 0, 1).then((res)=>{
+        ctx.replyWithPhoto(chat.id,res.photos[0][0].file_id,{caption: `${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,
             parse_mode:'HTML',
             reply_markup:{
                 inline_keyboard:[

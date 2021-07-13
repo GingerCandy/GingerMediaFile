969009681a13ebdf33ae73bf81c1a5d78d003191
BimoSora <bimosora3@gmail.com>
2021-07-13 08:32:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ bot.start(async(ctx)=>{
 
     if(length == 1){
         var profile = await bot.telegram.getUserProfilePhotos(ctx.chat.id)
-        if (profile.total_count == 0) return
+        if (profile.total_count == 0){
         ctx.reply(`${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,
             parse_mode:'HTML',
             reply_markup:{
@@ -48,7 +48,7 @@ bot.start(async(ctx)=>{
                 ]
             }
         )
-        
+        }else{
         ctx.replyWithPhoto(profile.photos[0][0].file_id,{caption: `${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,
             parse_mode:'HTML',
             reply_markup:{
@@ -59,6 +59,7 @@ bot.start(async(ctx)=>{
                 ]
             }
         })
+        }
     }else{
         file = await saver.getFile(query).then((res)=>{
             console.log(res);

98dbebe99a7d782e0e3f9db858cdf33313d73456
BimoSora <bimosora3@gmail.com>
2021-07-14 13:29:08 +0700

    Upload file

--- index.js
+++ index.js
@@ -328,44 +328,21 @@ bot.on('document', async (ctx) => {
         uniqueId: document.file_unique_id,
         type: 'document'
     }
+    console.log(fileDetails.caption);
+    await saver.checkBan(`${ctx.from.id}`).then((res) => {
+        console.log(res);
+        if (res == true) {
+            ctx.reply('⚠ANDA DILARANG KARENA MENYALAHGUNAKAN BOT, HUBUNGI ADMIN UNTUK BANDING')
+        } else {
+            saver.saveFile(fileDetails)
+            ctx.reply(`https://t.me/${process.env.BOTUSERNAME}?start=${document.file_unique_id}`)
+            ctx.replyWithDocument(document.file_id, {
+                chat_id: process.env.LOG_CHANNEL,
+                caption: `${ctx.message.caption}\n\nDari: ${ctx.from.id}\nNama depan: ${ctx.from.first_name}\nID file: ${document.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${document.file_unique_id}`
+            })
+        }
+    })
 
-    var member3 = await bot.telegram.getChatMember(-1001590114101, ctx.from.id)
-    console.log(member3);
-    if (!member3 || member3.status == 'left'){
-        var profile6 = await bot.telegram.getUserProfilePhotos(ctx.chat.id)
-        if (!profile6 || profile6.total_count == 0)
-        return ctx.reply(`${ctx.from.first_name} \n\nAnda belum masuk, silakan masuk dulu!`,{
-            parse_mode:'HTML',
-            reply_markup:{
-                inline_keyboard:[
-                    [{text:'Gabung Channel', url: 'https://t.me/gingercandyfiles'}]
-                ]
-            }
-        })
-        ctx.replyWithPhoto(profile6.photos[0][0].file_id,{caption: `${ctx.from.first_name} \n\nAnda belum masuk, silakan masuk dulu!`,
-            parse_mode:'HTML',
-            reply_markup:{
-                inline_keyboard:[
-                    [{text:'Gabung Channel', url: 'https://t.me/gingercandyfiles'}]
-                ]
-            }
-        })
-    }else{
-        console.log(fileDetails.caption);
-        await saver.checkBan(`${ctx.from.id}`).then((res) => {
-            console.log(res);
-            if (res == true) {
-                ctx.reply('⚠ANDA DILARANG KARENA MENYALAHGUNAKAN BOT, HUBUNGI ADMIN UNTUK BANDING')
-            } else {
-                saver.saveFile(fileDetails)
-                ctx.reply(`https://t.me/${process.env.BOTUSERNAME}?start=${document.file_unique_id}`)
-                ctx.replyWithDocument(document.file_id, {
-                    chat_id: process.env.LOG_CHANNEL,
-                    caption: `${ctx.message.caption}\n\nDari: ${ctx.from.id}\nNama depan: ${ctx.from.first_name}\nID file: ${document.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${document.file_unique_id}`
-                })
-            }
-        })
-    }
 })
 
 //video files
@@ -415,12 +392,18 @@ bot.on('photo', async(ctx) => {
         if (res == true) {
             ctx.reply('⚠ANDA DILARANG KARENA MENYALAHGUNAKAN BOT, HUBUNGI ADMIN UNTUK BANDING')
         } else {
-            saver.saveFile(fileDetails)
-            ctx.reply(`https://t.me/${process.env.BOTUSERNAME}?start=${photo[1].file_unique_id}`)
-            ctx.replyWithPhoto(photo[1].file_id, {
-                chat_id: process.env.LOG_CHANNEL,
-                caption: `${ctx.message.caption}\n\nDari: ${ctx.from.id}\nNama depan: ${ctx.from.first_name}\nID file: ${document.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${photo[1].file_unique_id}`
-            })
+            var member = await bot.telegram.getChatMember(-1001590114101, ctx.from.id)
+            console.log(member3);
+            if (!member3 || member3.status == 'left'){
+                ctx.reply(`Masuk dulu yuk`)
+            }else{
+                saver.saveFile(fileDetails)
+                ctx.reply(`https://t.me/${process.env.BOTUSERNAME}?start=${photo[1].file_unique_id}`)
+                ctx.replyWithPhoto(photo[1].file_id, {
+                    chat_id: process.env.LOG_CHANNEL,
+                    caption: `${ctx.message.caption}\n\nDari: ${ctx.from.id}\nNama depan: ${ctx.from.first_name}\nID file: ${document.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${photo[1].file_unique_id}`
+                })
+            }
         }
     })
 

0fb693a54ae6e915103b86e829a5d61f6cf8a0f5
BimoSora <bimosora3@gmail.com>
2021-07-15 04:41:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,7 @@ bot.start(async(ctx)=>{
         userId:ctx.from.id
     }
 
-    const myData = function(){
-        console.log('${ctx.from.first_name}');
+   var myData = await (${ctx.from.first_name})
     }
 
     if(ctx.from.id ==process.env.ADMIN){
@@ -40,7 +39,7 @@ bot.start(async(ctx)=>{
         if(length == 1){
             var profile = await bot.telegram.getUserProfilePhotos(ctx.chat.id)
             if (!profile || profile.total_count == 0)
-                return ctx.reply(`${myData} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,{
+                return ctx.reply(`$(myData} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,{
                 parse_mode:'HTML',
                 reply_markup:{
                    inline_keyboard:[

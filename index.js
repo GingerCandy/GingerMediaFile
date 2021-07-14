3186242028151bc9a4879ea5f654fba580c0ae10
BimoSora <bimosora3@gmail.com>
2021-07-14 20:37:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ db.connect((err) => {
     else { console.log('db connected'); }
 })
 
+function myData() { 
+   return
+   reply_markup:{
+     inline_keyboard:[
+          [{text:'Pencarian',switch_inline_query:''},{text:'Tautan',callback_data:'POP'}],
+          [{text:'Owner Bot', url: 'https://t.me/SoraHearts'},{text:'Owner Channel', url: 'https://t.me/Gingercandy02'}],
+          [{text:'Gabung Channel', url: 'https://t.me/gingercandyfiles'}]
+     ];
+}
+console.log(myData());
+
 //BOT START
 bot.start(async(ctx)=>{
 
@@ -39,11 +50,7 @@ bot.start(async(ctx)=>{
                 return ctx.reply(`${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,{
                 parse_mode:'HTML',
                 reply_markup:{
-                    inline_keyboard:[
-                        [{text:'Pencarian',switch_inline_query:''},{text:'Tautan',callback_data:'POP'}],
-                        [{text:'Owner Bot', url: 'https://t.me/SoraHearts'},{text:'Owner Channel', url: 'https://t.me/Gingercandy02'}],
-                        [{text:'Gabung Channel', url: 'https://t.me/gingercandyfiles'}]
-                    ]
+                    myData();
                 }
             })
                 ctx.replyWithPhoto(profile.photos[0][0].file_id,{caption: `${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,

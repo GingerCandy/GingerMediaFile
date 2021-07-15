0216bc950a7ec674a998bf4c0e452146f7c93e8c
BimoSora <bimosora3@gmail.com>
2021-07-15 12:08:35 +0700

    Upload file

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ db.connect((err) => {
 //ID Channel/Group
 const channelId = -1001590114101;
 
+function first_name2(ctx){
+    return `${ctx.from.first_name ? ctx.from.first_name : ""}`;
+}
+
 // inline keyboard
 const inKey = [
   [{text:'Pencarian',switch_inline_query:''},{text:'Tautan',callback_data:'POP'}],
@@ -50,7 +54,7 @@ bot.start(async(ctx)=>{
         if(length == 1){
             var profile = await bot.telegram.getUserProfilePhotos(ctx.chat.id)
             if (!profile || profile.total_count == 0)
-                return ctx.reply(`${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,{
+                return ctx.reply(`${first_name2()} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,{
                 parse_mode:'HTML',
                 reply_markup:{
                    inline_keyboard:inKey

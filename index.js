080025307c2aa60da241d5151cdb38a9393fe16d
BimoSora <bimosora3@gmail.com>
2021-07-15 11:53:02 +0700

    Upload file

--- index.js
+++ index.js
@@ -19,8 +19,16 @@ db.connect((err) => {
 //ID Channel/Group
 const channelId = -1001590114101;
 
-var myData = function(){
-    return `${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`;
+var arr =   `${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,{
+                parse_mode:'HTML',
+                reply_markup:{
+                inline_keyboard:inKey
+                }
+            }
+
+function _arrReply(ctx,arr){
+    var opt = {...} //ini opt tetap
+    arr.map(text => ctx.reply(text,opt)) 
 }
 
 // inline keyboard
@@ -54,12 +62,7 @@ bot.start(async(ctx)=>{
         if(length == 1){
             var profile = await bot.telegram.getUserProfilePhotos(ctx.chat.id)
             if (!profile || profile.total_count == 0)
-            ctx.reply(myData(),{
-                parse_mode:'HTML',
-                reply_markup:{
-                   inline_keyboard:inKey
-                }
-            })
+            return _arrReply(ctx,text);
             ctx.replyWithPhoto(profile.photos[0][0].file_id,{caption: `${ctx.from.first_name} \n\nSaya akan menyimpan file untuk Anda dan memberikan tautan yang dapat dibagikan, saya juga dapat membuat file tersedia untuk semua pengguna. Bot mendukung pencarian dan <a href="t.me/mdtohtmlbot">HTML</a>.`,
                 parse_mode:'HTML',
                 reply_markup:{

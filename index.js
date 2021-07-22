f8e4ede3bf77f974d650d2e73268194502012869
GingerCandy <87468539+GingerCandy@users.noreply.github.com>
2021-07-22 08:47:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -131,8 +131,6 @@ bot.start(async(ctx)=>{
                     }            
                 })
             }
-            //saving user details to the database
-            saver.saveUser(user)
         }else{
             try {
                 var botStatus = await bot.telegram.getChatMember(channelId, ctx.botInfo.id)
@@ -200,15 +198,14 @@ bot.start(async(ctx)=>{
                                 }            
                             })
                         }
-
-                        //saving user details to the database
-                        saver.saveUser(user)
                     }
                 }
             catch(error){
                 ctx.reply(`${messagebotnoaddgroup(ctx)}`)
             }
         }
+        //saving user details to the database
+        saver.saveUser(user)
     }
 })
 

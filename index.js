b247a7d7e23ab52a3241acc44edb911a60ac9d42
GingerCandy <87468539+GingerCandy@users.noreply.github.com>
2021-07-22 18:35:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.TOKEN)
 
+process.env.TZ = "Asia/Jakarta";
 
 //database
 const db = require('./config/connection')

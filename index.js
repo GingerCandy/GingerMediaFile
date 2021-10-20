a8be580ee2f86c9695ebbaeb2ce5098386d90955
GingerCandy <87468539+GingerCandy@users.noreply.github.com>
2021-10-20 17:03:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const mediaLimitConfig = {
     }
 }
 const bot = new Telegraf(process.env.TOKEN)
-bot.use(rateLimit(limitConfig))
+//bot.use(rateLimit(limitConfig))
 
 process.env.TZ = "Asia/Jakarta";
 

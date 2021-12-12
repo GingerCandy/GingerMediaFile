e34a180639d9fd9609938b86b08d66ccf7d51b37
BimoSora <bimosora3@gmail.com>
2021-12-12 20:45:32 +0700

    first commit

--- index.js
+++ index.js
@@ -109,6 +109,7 @@ bot.start(async(ctx)=>{
         if(ctx.from.id == process.env.ADMIN || ctx.from.id == process.env.ADMIN1 || ctx.from.id == process.env.ADMIN2){
             //welcoming message on /start and ifthere is a query available we can send files
             if(length == 1){
+                ctx.deleteMessage()
                 const profile = await bot.telegram.getUserProfilePhotos(ctx.from.id)
                 if(!profile || profile.total_count == 0)
                     return ctx.reply(`<a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a> \n\n${messagewelcome(ctx)}`,{
@@ -141,9 +142,11 @@ bot.start(async(ctx)=>{
                                 parse_mode:'HTML'
                             })
                         }
+                        ctx.deleteMessage()
                         await ctx.telegram.sendMediaGroup(ctx.chat.id, mediagroup);
                         setTimeout(captionFunction, 1000)
                     }catch(error){
+                        ctx.deleteMessage()
                         ctx.reply(`Media tidak ditemukan atau sudah dihapus`)
                     }
                 }else{
@@ -157,6 +160,7 @@ bot.start(async(ctx)=>{
                             })
                         }
                         if(res2.type=='video'){
+                            ctx.deleteMessage()
                             if(!res2.caption) {
                                 setTimeout(captionFunction2, 1000)
                                 return ctx.replyWithVideo(res2.file_id);
@@ -166,6 +170,7 @@ bot.start(async(ctx)=>{
                             });
                                 setTimeout(captionFunction2, 1000)
                         }else if(res2.type=='photo'){
+                            ctx.deleteMessage()
                             if(!res2.caption) {
                                 setTimeout(captionFunction2, 1000)
                                 return ctx.replyWithPhoto(res2.file_id);
@@ -175,6 +180,7 @@ bot.start(async(ctx)=>{
                             });
                                 setTimeout(captionFunction2, 1000)
                         }else if(res2.type=='document'){
+                            ctx.deleteMessage()
                             if(!res2.caption) {
                                 setTimeout(captionFunction2, 1000)
                                 return ctx.replyWithDocument(res2.file_id);
@@ -185,6 +191,7 @@ bot.start(async(ctx)=>{
                                 setTimeout(captionFunction2, 1000)
                         }
                     }catch(error){
+                        ctx.deleteMessage()
                         ctx.reply(`Media tidak ditemukan atau sudah dihapus`)
                     }
                 }
@@ -200,9 +207,11 @@ bot.start(async(ctx)=>{
                         //console.log(res);
                         if(res == true) {
                             if(ctx.chat.type == 'private') {
+                                ctx.deleteMessage()
                                 ctx.reply(`${messagebanned(ctx)}`)
                             }
                         }else{
+                            ctx.deleteMessage()
                             if(!profile2 || profile2.total_count == 0)
                                 return ctx.reply(`<a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a> \n\n${welcomejoin(ctx)}`,{
                                     parse_mode:'HTML',
@@ -223,14 +232,17 @@ bot.start(async(ctx)=>{
                 }else{
                     //welcoming message on /start and ifthere is a query available we can send files
                     if(length == 1){
+                        ctx.deleteMessage()
                         const profile3 = await bot.telegram.getUserProfilePhotos(ctx.from.id)
                             await saver.checkBan(`${ctx.from.id}`).then((res) => {
                                 //console.log(res);
                                 if(res == true) {
                                     if(ctx.chat.type == 'private') {
+                                        ctx.deleteMessage()
                                         ctx.reply(`${messagebanned(ctx)}`)
                                     }
                                 }else{
+                                    ctx.deleteMessage()
                                     if(!profile3 || profile3.total_count == 0)
                                         return ctx.reply(`<a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a> \n\n${messagewelcome(ctx)}`,{
                                             parse_mode:'HTML',
@@ -267,10 +279,12 @@ bot.start(async(ctx)=>{
                                     await saver.checkBan(`${ctx.from.id}`).then((res) => {
                                         //console.log(res);
                                         if(res == true) {
+                                            ctx.deleteMessage()
                                             if(ctx.chat.type == 'private') {
                                                 ctx.reply(`${messagebanned(ctx)}`)
                                             }
                                         }else{
+                                            ctx.deleteMessage()
                                             ctx.telegram.sendMediaGroup(ctx.chat.id, mediagroup);
                                             setTimeout(captionFunction, 1000)
                                         }
@@ -280,9 +294,11 @@ bot.start(async(ctx)=>{
                                         //console.log(res);
                                         if(res == true) {
                                             if(ctx.chat.type == 'private') {
+                                                ctx.deleteMessage()
                                                 ctx.reply(`${messagebanned(ctx)}`)
                                             }
                                         }else{
+                                            ctx.deleteMessage()
                                             ctx.reply(`Media tidak ditemukan atau sudah dihapus`)
                                         }
                                     })
@@ -301,10 +317,12 @@ bot.start(async(ctx)=>{
                                         //console.log(res);
                                         if(res == true) {
                                             if(ctx.chat.type == 'private') {
+                                                ctx.deleteMessage()
                                                 ctx.reply(`${messagebanned(ctx)}`)
                                             }
                                         }else{
                                             if(res2.type=='video'){
+                                                ctx.deleteMessage()
                                                 if(!res2.caption) {
                                                     setTimeout(captionFunction2, 1000)
                                                     return ctx.replyWithVideo(res2.file_id);
@@ -314,6 +332,7 @@ bot.start(async(ctx)=>{
                                                 });
                                                     setTimeout(captionFunction2, 1000)
                                             }else if(res2.type=='photo'){
+                                                ctx.deleteMessage()
                                                 if(!res2.caption) {
                                                     setTimeout(captionFunction2, 1000)
                                                     return ctx.replyWithPhoto(res2.file_id);
@@ -323,6 +342,7 @@ bot.start(async(ctx)=>{
                                                 });
                                                     setTimeout(captionFunction2, 1000)
                                             }else if(res2.type=='document'){
+                                                ctx.deleteMessage()
                                                 if(!res2.caption) {
                                                     setTimeout(captionFunction2, 1000)
                                                     return ctx.replyWithDocument(res2.file_id);
@@ -339,9 +359,11 @@ bot.start(async(ctx)=>{
                                         //console.log(res);
                                         if(res == true) {
                                             if(ctx.chat.type == 'private') {
+                                                ctx.deleteMessage()
                                                 ctx.reply(`${messagebanned(ctx)}`)
                                             }
                                         }else{
+                                            ctx.deleteMessage()
                                             ctx.reply(`Media tidak ditemukan atau sudah dihapus`)
                                         }
                                     })
@@ -351,6 +373,7 @@ bot.start(async(ctx)=>{
                     }
                 }
             catch(error){
+                ctx.deleteMessage()
                 ctx.reply(`${messagebotnoaddgroup(ctx)}`)
             }
         }
